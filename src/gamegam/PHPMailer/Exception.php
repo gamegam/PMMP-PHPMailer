cb59af466e599c92a760e7db334c7c3241be916e
gamegam <48847058+gamegam@users.noreply.github.com>
2025-11-04 01:58:15 +0900

    Add files via upload

---
+++ src/gamegam/PHPMailer/Exception.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace gamegam\PHPMailer;
+
+class Exception extends \Exception
+{
+    public function errorMessage()
+    {
+        return '<strong>' . htmlspecialchars($this->getMessage(), ENT_COMPAT | ENT_HTML401) . "</strong><br />\n";
+    }
+}
